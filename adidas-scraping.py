253953f86a34300548244031359b1235b370ed7d
sencho <senchoparameswara@gmail.com>
2022-03-11 14:53:07 +0700

    new code

--- adidas-scraping.py
+++ adidas-scraping.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 s = Service(r'D:\Project\RemoteWork\selenium-test\Browsers\chromedriver.exe')
@@ -12,5 +14,27 @@ driver.maximize_window()
 driver.find_element(by=By.XPATH, value='//*[@id="email"]').send_keys('agustyast@gmail.com')
 driver.find_element(by=By.XPATH, value='//*[@id="password"]').send_keys('Scrap131')
 time.sleep(10)
-
 driver.find_element(by=By.XPATH, value='//*[@id="root"]/main/div/div/div/div[1]/form/div[3]/button').click()
+
+driver.get('https://www.adidas.co.id/pria.html?sortKey=recommended_score&sortDirection=DESC')
+
+count = 1
+while True:
+    print(count)
+
+    main = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/main/section/div/div/div[4]/div/ul')))
+    main = driver.find_elements(By.CLASS_NAME, 'ProductCard ')
+
+    for i in main:
+            detail = i.find_element(By.CLASS_NAME, 'gl-product-card__details-main')
+            title = detail.find_element(By.TAG_NAME, 'span')
+            price = detail.find_element(By.TAG_NAME, 'div')
+            print(title.text, price.text)
+
+    nextpage = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#root > main > section > div > div > div.CategoryPage-ProductListWrapper > div > nav > div > li:nth-child(3) > a > span')))
+    nextpage.click()
+    time.sleep(20)
+    count += 1
+
+
--- auntminnie-login.py
+++ auntminnie-login.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 print('started program')
 
 s = Service(r'D:\Project\RemoteWork\selenium-test\Browsers\chromedriver.exe')
@@ -10,19 +13,34 @@ driver = webdriver.Chrome(service=s)
 
 driver.maximize_window()
 driver.delete_all_cookies()
+"""
 driver.get('https://www.auntminnie.com/index.aspx?sec=log')
 time.sleep(5)
+"""
 
-# fill username
+"""
+# fill login form
 driver.find_element(by=By.XPATH, value='//*[@id="ctl00_ctl00_pnlOutputText_Area2_ctl00_Login1_txtCN"]').send_keys('senchoparameswara')
-time.sleep(5)
-
-# fill password
 driver.find_element(by=By.XPATH, value='//*[@id="ctl00_ctl00_pnlOutputText_Area2_ctl00_Login1_txtPassword"]').send_keys('xxxtripl3x')
-time.sleep(5)
-
-# login
+time.sleep(10)
 driver.find_element(by=By.XPATH, value='//*[@id="ctl00_ctl00_pnlOutputText_Area2_ctl00_Login1_cmdSignIn"]').click()
+"""
+
+# get news item
+driver.get('https://www.auntminnie.com/index.aspx?sec=nws&sub=rad')
+
+
+# scraping data
+while True:
+    # handling error
+    main = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'MoreNewsItem')))
+    for i in main:
+        title = i.find_element(by=By.CLASS_NAME, value='Head')
+        link = title.find_element(by=By.TAG_NAME, value='a').get_attribute('href')
+        print(title.text, link)
+    time.sleep(5)
+    driver.find_element(by=By.LINK_TEXT, value='next »').click()
+
 time.sleep(20)
 
 driver.close()
