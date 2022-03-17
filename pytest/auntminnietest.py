bbdd020ea1e71e5c686c7b4b639fb411456ff9cb
sencho <senchoparameswara@gmail.com>
2022-03-17 15:45:10 +0700

    new code

--- pytest/auntminnietest.py
+++ pytest/auntminnietest.py
@@ -7,16 +7,22 @@ from selenium.webdriver.common.by import By
 
 class Auntminnietest(unittest.TestCase):
     def test_Title(self):
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        self.driver.get('https://www.auntminnie.com/')
-        print(f'Title of the main page: {self.driver.title}')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get('https://www.auntminnie.com/')
+        print(f'Title of the main page: {driver.title}')
+        assert driver.title == 'AuntMinnie.com - Radiology News and Education'
 
     def test_Casetitle(self):
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        self.driver.get('https://www.auntminnie.com/')
-        self.driver.find_element(By.XPATH, '//*[@id="nav"]/div[1]/ul/li[3]/a').click()
-        print(f'Title of the cases page: {self.driver.title}')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get('https://www.auntminnie.com/')
+        driver.find_element(By.XPATH, '//*[@id="nav"]/div[1]/ul/li[3]/a').click()
+        print(f'Title of the cases page: {driver.title}')
+        assert driver.title == 'Radiology Education, Case Study'
 
+    def test_Jobstitle(self):
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get('https://www.auntminnie.com/')
+        driver.find_element(By.XPATH, '//*[@id="nav"]/div[1]/ul/li[9]/a').click()
+        print(f'Title of the jobs page: {driver.title}')
+        assert driver.title == 'Radiology Jobs'
 
-if __name__ == 'main':
-    unittest.main()
--- pytest/testcase1.py
+++ pytest/testcase1.py
@@ -2,8 +2,16 @@ import unittest
 
 
 class Test(unittest.TestCase):
+    def setup(self):
+        print('setup')
+
     def test_first(self):
         print('This is my first test case')
+        assert True
+
+    def test_second(self):
+        print('This is second test case')
+        assert False
 
 
 if __name__ == '__main__':
